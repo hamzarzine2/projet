c5175bad2899c79ea22d2aee23b0de55225f406c
hamzarzine2 <hamza.rzine@student.vinci.be>
2023-03-06 14:12:15 +0100

    init

---
+++ src/Graph.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class Graph {
+
+  File lignes,troncons;
+  public Graph(File lignes, File troncons) {
+    this.lignes=lignes;
+    this.troncons=troncons;
+  }
+
+  public void calculerCheminMinimisantNombreTroncons(String boileau, String alma) {
+
+  }
+
+  public void calculerCheminMinimisantTempsTransport(String boileau, String alma) {
+  }
+}
---
+++ src/domaine/Station.java
@@ -0,0 +1,10 @@
+package domaine;
+
+public class Station {
+
+  String nom;
+
+  public Station(String nom) {
+    this.nom = nom;
+  }
+}
